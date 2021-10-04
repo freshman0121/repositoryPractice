0b5ccab9eb599d9c4eed9eb1a707c49c2963e6e6
Khalil Jackson <khaliljackson@MacBook-Pro.local>
2021-10-04 07:36:11 -0400

    //Initial commit doo doo code that is just practice

---
+++ src/repositoryPractice.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class repositoryPractice {
+    static final int addition = 1;
+    static final double division = 0.5;
+
+    public static void main(String[] args) {
+
+        int age;
+
+        age = keyboard.nextInt();
+        System.out.print("Welcome to repository practice!" + age);
+
+
+
+    }
+
+}
